149678bf22298857b877dc41c118618fc7e048da
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-02-27 16:49:18 +0200

    todo3

--- create.php
+++ create.php
@@ -1,13 +1,15 @@
 <?php
 // kuka tahansa saa kutsua palvelua:
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST,OPTIONS');
+header('Access-Control-Allow-Headers: Accept,Content-Type,Access-Control-Allow-Header');
 // palautettava tieto on JSON-muodossa:
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     return 0;
 }
-
+ //luetaan tieto kentästä; FILTER_SANITIZE_STRING - tekstille
 $input = json_decode(file_get_contents('php://input'));
 $description = filter_var($input->description,FILTER_SANITIZE_STRING);
 
@@ -16,6 +18,7 @@ try {
     $db = new PDO('mysql:host=localhost;dbname=todo;charset=utf8','root','');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
+    //sql lause
     $query = $db->prepare('insert into task(description) value (:description)');
     $query->bindValue(':description', $description,PDO::PARAM_STR);
     $query->execute();
